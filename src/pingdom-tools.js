8c7c6db19e1b802eefcec97e1013914ecf20018c
Chris Burgess <chrisburgess@catalyst.net.nz>
2018-08-25 16:42:09 +1200

    🛠 Javascript ...

--- src/pingdom-tools.js
+++ src/pingdom-tools.js
@@ -10,74 +10,90 @@
 // Commands:
 //   hubot is <domain> (fast|slow)? - Request a Pingdom pagetest report on <domain>
 //   hubot (pagetest|fpt) <domain> - Request a Pingdom pagetest report on <domain>
-const pkg = require('../package.json');
 
-const PingdomTools = {
-  intervals: {},
+class PingdomTools {
+  constructor(robot) {
+    let intervals = robot.brain.get('pingdom-tools-intervals')
+    if (typeof intervals === 'undefined') {
+      intervals = {}
+    }
+  }
 
-  full_page_test: (msg) => {
+  full_page_test(msg) {
     var fpt_site, fpt_url, my;
-    my = this;
     fpt_url = 'https://fpt-api.pingdom.com/api/0.1/test?save=false&url=';
-    fpt_site = msg.match[1];
-    return msg.http(`${fpt_url}${fpt_site}`).header('referer', 'https://tools.pingdom.com/').header('user-agent', `hubot-pingdom-tools ${pkg.version} (${pkg.homepage})`).get()((err, res, body) => {
-      if (err) {
-        msg.send('Error: ' + err);
-        return;
-      }
-      if (body) {
-        if (res = JSON.parse(body)) {
-          if (res.poll_state_url) {
-            robot.logger.debug(this.intervals);
-            return this.intervals[res.poll_state_url] = setInterval(this.show_result, 2000, res.poll_state_url, msg);
+    fpt_site = msg.match[2];
+    const pkg = require('../package.json')
+    return msg.http(`${fpt_url}${fpt_site}`)
+      .header('referer', 'https://tools.pingdom.com/')
+      .header('user-agent', `hubot-pingdom-tools ${pkg.version} (${pkg.homepage})`)
+      .get()((err, res, body) => {
+        if (err) {
+          msg.send('Error: ' + err);
+          return;
+        }
+        if (body) {
+          if (res = JSON.parse(body)) {
+            if (res.poll_state_url) {
+              msg.fpt_interval = setInterval(this.show_result, 2000, res.poll_state_url, msg)
+            }
           }
         }
-      }
     });
-  },
+  }
 
-  show_result: (url, msg) => {
-    return msg.http(url).header('referer', 'https://tools.pingdom.com/').header('user-agent', `hubot-pingdom-tools ${pkg.version} (${pkg.homepage})`).get()((err, res, poll_body) => {
+  show_result(url, msg) {
+    const pkg = require('../package.json');
+    return msg.http(url)
+      .header('referer', 'https://tools.pingdom.com/')
+      .header('user-agent', `hubot-pingdom-tools ${pkg.version} (${pkg.homepage})`)
+      .get()((err, res, poll_body) => {
+
       var poll_res, replies, result, slower_than;
       if (poll_body) {
-        poll_res = JSON.parse(poll_body);
+        poll_res = JSON.parse(poll_body)
         if (poll_res && poll_res.results && poll_res.state === 'completed') {
-          result = poll_res.results;
-          replies = [];
+          result = poll_res.results
+          replies = []
           if (result.report_url) {
-            url = result.report_url.replace(/\.com\#!/, '.com/#!') + `/${msg.match[1]}`;
-            replies.push(`Full page report: ${url}`);
+            url = result.report_url.replace(/\.com\#!/, '.com/#!') + `/${msg.match[1]}`
+            replies.push(`Full page report: ${url}`)
           }
           if (result.pagespeed_score && result.load_time_percentile) {
             if (result.load_time_percentile >= 50) {
-              replies.push(`Pagespeed score ${result.pagespeed_score}, faster than ${result.load_time_percentile}% of sites.`);
+              replies.push(`Pagespeed score ${result.pagespeed_score}, faster than ${result.load_time_percentile}% of sites.`)
             } else {
-              slower_than = -result.load_time_percentile + 100;
-              replies.push(`Pagespeed score ${result.pagespeed_score}, slower than ${slower_than}% of sites.`);
+              slower_than = -result.load_time_percentile + 100
+              replies.push(`Pagespeed score ${result.pagespeed_score}, slower than ${slower_than}% of sites.`)
             }
           }
           if (result.page_bytes && result.page_load_time) {
-            replies.push(`${result.page_bytes} bytes in ${result.page_load_time} ms.`);
+            replies.push(`${result.page_bytes} bytes in ${result.page_load_time} ms.`)
           }
           if (replies.length) {
-            msg.send(replies.join("\n"));
+            msg.send(replies.join("\n"))
           }
-          clearInterval(this.intervals[url]);
+
+          clearInterval(msg.fpt_interval)
         }
       }
       if (err) {
-        msg.send('Error running Pingdom Full Page Test on ' + url);
-        return clearInterval(this.intervals[url]);
+        msg.send('Error running Pingdom Full Page Test on ' + url)
+        robot.logger.error(err, 'Pingdom FPT Error')
+        clearInterval(msg.fpt_interval)
       }
-    });
+    })
   }
 }
 
 module.exports = function(robot) {
+  let pt = new PingdomTools(robot)
+
+  // robot.hear(/(wrms search|search wrms( for)?) (.*)$/i, (msg) => {
   robot.respond(/(pagetest|fpt) ([^ ]*)?$/i, function(msg) {
-    return PingdomTools.full_page_test(msg);
+    return pt.full_page_test(msg);
   });
   return robot.respond(/is ([^ ]*) (fast|slow)/i, function(msg) {
-    return PingdomTools.full_page_test(msg);
+    return pt.full_page_test(msg);
   });
 };
